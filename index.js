0f88e50357a156d74e36e3823568467f9ebaeffd
unknown <maliaga@gruporpp.com.pe>
2021-06-27 23:50:49 -0500

    set mysql

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
+const Mysql = require('mysql2')
 const Express = require('express')
 const App = Express()
 
-App.get("/", (req, res) => {
-    res.send("Ok")
+const Connection = Mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PWD,
+    database: process.env.DB,
+    port: process.env.DBPORT
+})
+
+Connection.connect()
+    
+App.get("/", async (req, res) => {
+    let status = 200
+    let message = ""
+    try {
+        let r = await new Promise((resolve, reject) => {
+            Connection.query("select now()", (err, results, fields) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(results)
+                }
+            })
+        })
+        message = r
+    } catch (e) {
+        status = 500
+        message = e.message
+    } finally {
+        res.status(status).json({message})
+    }
 })
 
 App.listen(process.env.PORT, (e) => {
@@ -12,3 +41,6 @@ App.listen(process.env.PORT, (e) => {
     }
     console.log("app running")
 })
+process.on('uncaughtException', function(error) {
+    process.exit(1)
+});
---
+++ mysql.sh
@@ -0,0 +1,7 @@
+docker run --name mysql \
+-p 3306:3306 \
+-e MYSQL_ROOT_PASSWORD=sesion5 \
+-e MYSQL_DATABASE=sesion5b \
+-e MYSQL_USER=sesion5User \
+-e MYSQL_PASSWORD=sesion5Pwd \
+mysql
